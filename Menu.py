b1c7fb54795a19b56b0bf8b2d7732f2f1017df8b
Roberto Dailey <dailey.roberto@gmail.com>
2016-10-10 12:10:13 -0500

    changes to menu setup

--- Menu.py
+++ Menu.py
@@ -14,19 +14,29 @@ while(exitMenu != True):
     if(choice == '1'):
         print('\nAvailible Websites: \n')
         print('   We\'re sorry, no currently availible websites \n')
+
+
     elif(choice == '2'):
         print('\nEnter Websites to Mine, comma dilimited')
         choice = input('websites must match url exactly* \n')
         print('\nWe\'re sorry, mining is currently unavailible \n')
+
+
     elif (choice == '3'):
         print('\n Daily miner is currently not availible \n')
+
+
     elif (choice == '4'):
         print('\n[1] correlation + resampling-based confidence intervals')
         print('[2] PCA Analysis')
         choice = input('Type the numeric of your choice, then press Enter \n')
         print('\ncurrently no analysis availible \n')
+
+
     elif (choice == '0'):
         exitMenu = True
+
+
     else:
         print('\n Non-valid input, please enter valid input: \n')
 
